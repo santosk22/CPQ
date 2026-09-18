09735e803d9866da7cc4065519de02a829e672ee
santosk22 <sharmasantoshk@yahoo.com>
2026-09-18 08:55:59 +0530

    Add files via upload

---
+++ .github/workflows/Beginner pro 1.py
@@ -0,0 +1,6 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print ("My name is Samarth")
+My name is Samarth
+>>> print (15+42)
+57
